bd8f2d8b3db0621795e7d63d3a0fc7d0c47180e6
Ayman <ayman4001@hotmail.com>
2016-11-25 19:34:52 -0700

    Vini changes

--- src/Event.java
+++ src/Event.java
@@ -1,52 +1,42 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.Serializable;
-import java.util.Date;
-
-
 /**
  * Created by elsrouay on 11/21/2016.
- * Serialization reference: http://www.tutorialspoint.com/java/java_serialization.htm
  */
-class Event
-        implements Serializable {
+
+// Edited by Vinicius Ferreira on 24 and 25 November 2016
+
+// VF: importing classes that will be used for the GUI
+import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
+
+public class Event {
     private Date date;
     private String Title;
-    private String startTime;
-    private String endTime;
+    private Date startTime;
+    private Date endTime;
+    // VF: variables declared in the beginning of development. Might require changes
 
+    // VF: Main method invokes the required methods to make windows show up. May be removed afterwards.
     public static void main(String[] args) {
         buildFrame();
         addEvent();
+        SpecialEvent specialEvent = new SpecialEvent();
+        specialEvent.buildEventWindow();
 
     }
 
+    // VF: this method builds the GUI frame when invoked.
     private static void buildFrame() {
         JFrame frame = new JFrame("Welcome to Day Routine App");
 
         try {
+
+            // VF: I did not change anything here, but the menu might prove unnecessary.
             //create menu and menu items
             JMenuBar menuBar = new JMenuBar();
             JMenu menuFile = new JMenu("File");
             JMenuItem menuItemExit = new JMenuItem("Exit");
-            final JMenuItem menuItemAdd = new JMenuItem("Add Event");
-
-            menuItemExit.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent actionEvent) {
-
-                    System.exit(0);
-                }
-            });
-
-            menuItemAdd.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent actionEvent) {
-                    addEvent();
-                }
-            });
-
-
+            JMenuItem menuItemAdd = new JMenuItem("Add Event");
             menuFile.add(menuItemAdd);
             menuFile.add(menuItemExit);
             menuBar.add(menuFile);
@@ -54,28 +44,213 @@ class Event
             frame.setJMenuBar(menuBar);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+            // VF: A few lines removed here. FlowLayout did not fit the requirements, so switched to GridLayout instead.
+
+            /*
             JPanel panel = new JPanel();
             panel.setLayout(new FlowLayout());
-            JLabel label = new JLabel("Day Routine can make your life easier");
-            JButton button = new JButton();
-            button.setText("Add");
-            button.setBackground(Color.CYAN);
-            JTextField textFieldEvent = new JTextField(50);
-            textFieldEvent.setSize(40, 10);
-            panel.add(textFieldEvent);
-            panel.add(label);
-            panel.add(button);
-            panel.add(textFieldEvent);
-            //adding the panel to the frame after adding all controls to the panel
-            frame.add(panel);
+            */
 
-            frame.setSize(500, 500);
+            // VF: a Grid Layout was defined with 7 rows and 10 columns.
+            GridLayout grid = new GridLayout(7, 10);
+            frame.setLayout(grid);
+            frame.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 
+            // VF: Rows 1 and 2 only have text components.
+
+            // VF: start of row 1
+            frame.add(new JLabel(""));
+            frame.add(new JLabel(""));
+            frame.add(new JLabel(""));
+            frame.add(new JLabel(""));
+            frame.add(new JLabel("Event"));
+            frame.add(new JLabel(""));
+            frame.add(new JLabel(""));
+            frame.add(new JLabel(""));
+            frame.add(new JLabel(""));
+            frame.add(new JLabel(""));
+
+            // VF: start of row 2
+            frame.add(new JLabel(""));
+            frame.add(new JLabel("Day"));
+            frame.add(new JLabel(""));
+            frame.add(new JLabel(""));
+            frame.add(new JLabel("Start"));
+            frame.add(new JLabel("End"));
+            frame.add(new JLabel("Description"));
+            frame.add(new JLabel("Type"));
+            frame.add(new JLabel(""));
+            frame.add(new JLabel(""));
+
+            // VF: start of row 3
+
+            // VF: buttons were added to change the displayed values when pressed. They are not working now.
+
+            JButton minusDay = new JButton();
+            minusDay.setText("Minus Day");
+            minusDay.setBackground(Color.RED);
+            frame.add(minusDay);
+
+            // VF: This value should display the date and be changed when buttons "+"/"-" are pressed.
+
+            JLabel dayLabel = new JLabel("22");
+            frame.add(dayLabel);
+
+            JButton plusDay = new JButton();
+            plusDay.setText("Plus Day");
+            plusDay.setBackground(Color.BLUE);
+            frame.add(plusDay);
+
+            frame.add(new JLabel(""));
+
+            // VF: Those are only fillers to mark where the actual information on the events of the day will be
+            //  displayed. I am pretty sure those objects will need to be changed, but I will keep as is,
+            //  which is enough for a placeholder.
+
+            JLabel startOne = new JLabel("start one");
+            frame.add(startOne);
+
+            JLabel endOne = new JLabel("end one");
+            frame.add(endOne);
+
+            JLabel descriptionOne = new JLabel("description one");
+            frame.add(descriptionOne);
+
+            JLabel typeOne = new JLabel("type one");
+            frame.add(typeOne);
+
+            frame.add(new JLabel(""));
+            frame.add(new JLabel(""));
+
+            // VF: start of row 4
+
+            frame.add(new JLabel(""));
+            frame.add(new JLabel("Month"));
+            frame.add(new JLabel(""));
+            frame.add(new JLabel(""));
+
+            JLabel startTwo = new JLabel("start two");
+            frame.add(startTwo);
+
+            JLabel endTwo = new JLabel("end two");
+            frame.add(endTwo);
+
+            JLabel descriptionTwo = new JLabel("description two");
+            frame.add(descriptionTwo);
+
+            JLabel typeTwo = new JLabel("type two");
+            frame.add(typeTwo);
+
+            frame.add(new JLabel(""));
+            frame.add(new JLabel(""));
+
+            // VF: start of row 5
+
+            JButton minusMonth = new JButton();
+            minusMonth.setText("Minus Month");
+            minusMonth.setBackground(Color.RED);
+            frame.add(minusMonth);
+
+            JLabel monthLabel = new JLabel("11");
+            frame.add(monthLabel);
+
+            JButton plusMonth = new JButton();
+            plusMonth.setText("Plus Month");
+            plusMonth.setBackground(Color.BLUE);
+            frame.add(plusMonth);
+
+            frame.add(new JLabel(""));
+
+            JLabel startThree = new JLabel("start three");
+            frame.add(startThree);
+
+            JLabel endThree = new JLabel("end three");
+            frame.add(endThree);
+
+            JLabel descriptionThree = new JLabel("description three");
+            frame.add(descriptionThree);
+
+            JLabel typeThree = new JLabel("type three");
+            frame.add(typeThree);
+
+            frame.add(new JLabel(""));
+
+            // VF: The Add Event, Edit Event and Remove Event buttons will call other windows. They are not working now.
+
+            JButton addEventButton = new JButton();
+            addEventButton.setText("Add Event");
+            addEventButton.setBackground(Color.BLUE);
+            frame.add(addEventButton);
+
+            // VF: start of row 6
+
+            frame.add(new JLabel(""));
+            frame.add(new JLabel("Year"));
+            frame.add(new JLabel(""));
+            frame.add(new JLabel(""));
+
+            JLabel startFour = new JLabel("start four");
+            frame.add(startFour);
+
+            JLabel endFour = new JLabel("end four");
+            frame.add(endFour);
+
+            JLabel descriptionFour = new JLabel("description four");
+            frame.add(descriptionFour);
+
+            JLabel typeFour = new JLabel("type four");
+            frame.add(typeFour);
+
+            frame.add(new JLabel(""));
+
+            JButton editEventButton = new JButton();
+            editEventButton.setText("edit Event");
+            editEventButton.setBackground(Color.YELLOW);
+            frame.add(editEventButton);
+
+            // VF: start of row 7
+
+            JButton minusYear = new JButton();
+            minusYear.setText("Minus Year");
+            minusYear.setBackground(Color.RED);
+            frame.add(minusYear);
+
+            JLabel yearLabel = new JLabel("2016");
+            frame.add(yearLabel);
+
+            JButton plusYear = new JButton();
+            plusYear.setText("Plus Year");
+            plusYear.setBackground(Color.BLUE);
+            frame.add(plusYear);
+
+            frame.add(new JLabel(""));
+
+            JLabel startFive = new JLabel("start five");
+            frame.add(startFive);
+
+            JLabel endFive = new JLabel("end five");
+            frame.add(endFive);
+
+            JLabel descriptionFive = new JLabel("description five");
+            frame.add(descriptionFive);
+
+            JLabel typeFive = new JLabel("type five");
+            frame.add(typeFive);
+
+            frame.add(new JLabel(""));
+
+            JButton removeEventButton = new JButton();
+            removeEventButton.setText("Remove Event");
+            removeEventButton.setBackground(Color.RED);
+            frame.add(removeEventButton);
+
+            // VF: end of rows.
+
+            frame.setSize(1000, 500);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
             frame.setVisible(true);
-
             addEvent();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -85,9 +260,7 @@ class Event
     private static void addEvent() {
 
         try {
-            serializeEvent.main(null);
-
-
+            System.out.println("Event added.");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -111,23 +284,19 @@ class Event
 
     }
 
-    public String getStartTime() {
+    public Date getStartTime() {
         return startTime;
-    }
-
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
     }
 
     public void setDate(Date date) {
         this.date = date;
     }
 
-    public String getEndTime() {
+    public Date getEndTime() {
         return endTime;
     }
 
-    public void setEndTime(String endTime) {
+    public void setEndTime(Date endTime) {
         this.endTime = endTime;
     }
 
@@ -136,7 +305,7 @@ class Event
     }
 
     public void setTitle(String title) {
-        this.Title = title;
+        Title = title;
     }
 
 }
--- src/SpecialEvent.java
+++ src/SpecialEvent.java
@@ -1,11 +1,19 @@
+import javafx.scene.Parent;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
+
 /**
  * Created by elsrouay on 11/22/2016.
  */
 
-class SpecialEvent extends Event {
+// Edited by Vinicius Ferreira on 26/11/2016.
+
+public class SpecialEvent extends Event{
     private String eventDescription;
 
-        public String getEventDescription() {
+    public String getEventDescription() {
         return eventDescription;
     }
 
@@ -14,5 +22,106 @@ class SpecialEvent extends Event {
 
     }
 
+    public void buildEventWindow() {
+        JFrame eventFrame = new JFrame("Change Events");
+        try {
+
+            eventFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            // VF: a Grid Layout was defined with 7 rows and 10 columns.
+
+            GridLayout grid = new GridLayout(6, 5, 10, 10);
+            eventFrame.setLayout(grid);
+            eventFrame.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+
+            // VF: start of row 1
+            eventFrame.add(new JLabel("Event Start Day"));
+            eventFrame.add(new JLabel("Event Start Month"));
+            eventFrame.add(new JLabel("Event Start Year"));
+            eventFrame.add(new JLabel("Event Start Hours"));
+            eventFrame.add(new JLabel("Event Start Minutes"));
+
+            // VF: start of row 2
+
+            JTextField startDay = new JTextField(2);
+            JTextField startMonth = new JTextField(2);
+            JTextField startYear = new JTextField(4);
+            JTextField startHours = new JTextField(2);
+            JTextField startMinutes = new JTextField(2);
+
+            eventFrame.add(startDay);
+            eventFrame.add(startMonth);
+            eventFrame.add(startYear);
+            eventFrame.add(startHours);
+            eventFrame.add(startMinutes);
+
+            // VF: start of row 3
+
+            eventFrame.add(new JLabel("Event End Day"));
+            eventFrame.add(new JLabel("Event End Month"));
+            eventFrame.add(new JLabel("Event End Year"));
+            eventFrame.add(new JLabel("Event End Hours"));
+            eventFrame.add(new JLabel("Event End Minutes"));
+
+            // VF: start of row 4
+
+            JTextField endDay = new JTextField(2);
+            JTextField endMonth = new JTextField(2);
+            JTextField endYear = new JTextField(4);
+            JTextField endHours = new JTextField(2);
+            JTextField endMinutes = new JTextField(2);
+
+            eventFrame.add(endDay);
+            eventFrame.add(endMonth);
+            eventFrame.add(endYear);
+            eventFrame.add(endHours);
+            eventFrame.add(endMinutes);
+
+            // VF: start of row 5
+
+            eventFrame.add(new JLabel("Event Description"));
+            eventFrame.add(new JLabel("Event Type"));
+            eventFrame.add(new JLabel(""));
+            eventFrame.add(new JLabel(""));
+
+            JButton confirm = new JButton();
+            confirm.setText("Confirm");
+            confirm.setBackground(Color.BLUE);
+            eventFrame.add(confirm);
+
+            // VF: start of row 6
+
+            JTextField eventDescription = new JTextField(20);
+            eventFrame.add(eventDescription);
+
+            JComboBox comboBox = new JComboBox();
+            comboBox.addItem("Eat");
+            comboBox.addItem("Pray");
+            comboBox.addItem("Love");
+
+            eventFrame.add(comboBox);
+
+            eventFrame.add(new JLabel(""));
+            eventFrame.add(new JLabel(""));
+
+            JButton cancel = new JButton();
+            cancel.setText("Cancel");
+            cancel.setBackground(Color.RED);
+            eventFrame.add(cancel);
+
+            // VF: end of rows.
+
+            eventFrame.setSize(1000, 500);
+            eventFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            eventFrame.setVisible(true);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
+
+
